9078a04351e75834882681dcfe22931fbb56f752
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-07 15:17:38 +0530

    Initial Commit

---
+++ ToDoListApp/ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Sagar Kadam on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
