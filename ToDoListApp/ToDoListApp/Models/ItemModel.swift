67650ebbeb285e9d10f9688425742a11c35336a1
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-23 17:57:18 +0530

    Save and persist data with UserDefaults | Todo List #4

--- ToDoListApp/ToDoListApp/Models/ItemModel.swift
+++ ToDoListApp/ToDoListApp/Models/ItemModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // Immuatable Struct
-struct ItemModel: Identifiable {
+struct ItemModel: Identifiable, Codable {
     let id: String
     let title: String
     let isCompleted: Bool
--- ToDoListApp/ToDoListApp/ViewModels/ListViewModel.swift
+++ ToDoListApp/ToDoListApp/ViewModels/ListViewModel.swift
@@ -18,19 +18,33 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items:[ItemModel] = []
+    @Published var items:[ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
+        /*
         let newItems = [
             ItemModel(title: "First Task", isCompleted: false),
             ItemModel(title: "This is Second Task", isCompleted: true),
             ItemModel(title: "Third!", isCompleted: false),
         ]
         items.append(contentsOf: newItems)
+         */
+        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -57,6 +71,12 @@ class ListViewModel: ObservableObject {
         if let index = items.firstIndex(where: {$0.id == item.id}) {
             items[index] = item.updateCompletion()
             
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
