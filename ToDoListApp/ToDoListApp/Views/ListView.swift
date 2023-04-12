d2faa2a54d3ad2f807d7e261d760f1d93acbd909
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-04-12 18:12:39 +0530

    Updated code

--- ToDoListApp/ToDoListApp/Views/ListView.swift
+++ ToDoListApp/ToDoListApp/Views/ListView.swift
@@ -12,19 +12,21 @@ struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
-        List {
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
+        ZStack {
+            List {
+                ForEach(listViewModel.items) { item in
+                    ListRowView(item: item)
+                        .onTapGesture {
+                            withAnimation(.linear) {
+                                listViewModel.updateItem(item: item)
+                            }
                         }
-                    }
+                }
+                .onDelete(perform: listViewModel.deleteItem)
+                .onMove(perform: listViewModel.moveItem)
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
+            .listStyle(PlainListStyle())
         }
-        .listStyle(PlainListStyle())
         .navigationTitle("To Do List 📝")
         .navigationBarItems(
             leading: EditButton(),
